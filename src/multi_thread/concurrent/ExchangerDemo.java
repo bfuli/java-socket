e4aa8212ffb0bbb289eccd51ad87468ed2588839
bfl <2405720158@qq.com>
2021-12-02 20:29:35 +0800

    commit

--- src/multi_thread/concurrent/ExchangerDemo.java
+++ src/multi_thread/concurrent/ExchangerDemo.java
@@ -1,2 +1,28 @@
-package multi_thread.concurrent;public class ExchangerDemo {
+package multi_thread.concurrent;
+
+import java.util.concurrent.Exchanger;
+
+public class ExchangerDemo {
+    public static void main(String[] args) throws InterruptedException {
+        Exchanger<String> exchanger = new Exchanger<>();
+        new Thread(()->{
+            try {
+                System.out.println("线程A，得到了另一个线程的数据："+ exchanger.exchange("这是来自线程A的数据"));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        System.out.println("这个时候线程A是阻塞的，在等待线程B的数据");
+        Thread.sleep(1000);
+
+        new Thread(()->{
+            try {
+                System.out.println("这是线程B，得到了另⼀个线程的数据："
+                        + exchanger.exchange("这是来⾃线程B的数据"));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
 }
